72c6cecb9e6c0a5e79507ff03b01079ca3dd1d5d
yelingjun <yljflj123>
2026-04-01 12:52:47 +0800

    feat: 新增用户确认支付的接口

--- evmoto-common-feign/src/main/java/com/evmoto/common/feign/account/AccountServiceClient.java
+++ evmoto-common-feign/src/main/java/com/evmoto/common/feign/account/AccountServiceClient.java
@@ -4,7 +4,9 @@ import com.evmoto.common.core.domain.R;
 import com.evmoto.common.feign.account.dto.DriverTodayIncomeDto;
 import com.evmoto.common.feign.constant.ServiceNameConstants;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
@@ -19,7 +21,19 @@ import java.util.List;
 @FeignClient(ServiceNameConstants.MB_CLOUD_ACCOUNT)
 public interface AccountServiceClient {
 
+    /**
+     * 获取当天收入
+     * @param driverIds
+     * @return
+     */
     @PostMapping("/account/feign/todayIncome/get")
     R<List<DriverTodayIncomeDto>> getDriverTodayIncome(@RequestBody List<Integer> driverIds);
 
+    /**
+     * 用户确认支付
+     * @param orderId
+     * @return
+     */
+    @PutMapping("/account/feign/user/pay/confirm/{orderId}")
+    R<Boolean> userConfirm(@PathVariable("orderId") Long orderId);
 }
